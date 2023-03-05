789c9ac229f97efaf3d645ccaf415db674435898
Jamie Bly <90733356+JamieBly@users.noreply.github.com>
2023-03-05 00:31:22 -0500

    Finished Base Dialog

--- Assets/DialogManager.cs
+++ Assets/DialogManager.cs
@@ -8,8 +8,9 @@ public class DialogManager : MonoBehaviour
     public static int MAX_NUMBER_OF_CHARS = 324; //Max number of characters used in the Dialog box before text leaves the area
 
     public TextMeshProUGUI dialogText;
-    
-    
+    int x = 200;
+
+
     // Start is called before the first frame update
     void Start()
     {   
@@ -21,13 +22,33 @@ public class DialogManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKey(KeyCode.Return))
         {
-           // dia.SetActive(true);
-            //transform.position = new Vector3(0.3692896f, -1.339681f, 0.02488603f);
-            transform.position = new Vector3(100000, 100000, 100000);
-    
-            dialogText.text = "dA";
+           if ( (dialogText.text == "") && (x >= 200) )
+            {
+                showDialog();
+                x = 0;
+            } else if ( (dialogText.text != "") && (x >= 200) )
+            {
+                hidDialog();
+                x = 0;
+            }
         }
+
+        x = x + 1;
+    }
+
+    void hidDialog()
+    {
+        transform.position = new Vector3(100000, 100000, 100000);
+
+        dialogText.text = "";
+    }
+
+    void showDialog()
+    {
+        transform.position = new Vector3(0.3692896f, -1.339681f, 0.02488603f);
+        dialogText.text = "Good";
     }
 }
